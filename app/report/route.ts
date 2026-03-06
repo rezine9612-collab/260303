6b4bf586a4bcaf6b815d3baec399b26746af8606
rezine9612-collab <rezine9612@gmail.com>
2026-03-06 22:41:47 +0900

    Update route.ts

--- app/report/route.ts
+++ app/report/route.ts
@@ -1,6 +1,12 @@
 import { NextResponse } from "next/server"
+import fs from "fs"
+import path from "path"
 
 export async function GET() {
+
+  const reportPath = path.join(process.cwd(), "public", "report.PATCHED_v3.html")
+
+  let html = fs.readFileSync(reportPath, "utf8")
 
   const reportJSON = {
     meta: {},
@@ -10,31 +16,17 @@ export async function GET() {
     rfs: {}
   }
 
-  const html = `
-<!DOCTYPE html>
-<html>
-<head>
-<meta charset="utf-8">
-<title>NeuPrint Report</title>
-</head>
-
-<body>
-
-<div id="report-root"></div>
-
-<script id="dev-report-json" type="application/json">
-${JSON.stringify(reportJSON)}
-</script>
-
-<script src="/report.js"></script>
-
-</body>
-</html>
-`
+  html = html.replace(
+    `<script id="dev-report-json" type="application/json">
+</script>`,
+    `<script id="dev-report-json" type="application/json">
+${JSON.stringify(reportJSON, null, 2)}
+</script>`
+  )
 
   return new NextResponse(html,{
     headers:{
-      "Content-Type":"text/html"
+      "Content-Type":"text/html; charset=utf-8"
     }
   })
 
